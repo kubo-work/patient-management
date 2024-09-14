c33d30a8f969efbbe103ce337f295bce04a885a4
kubo-work <kubowork0501@gmail.com>
2024-09-14 21:21:26 +0900

    ✨ doctor 基本レイアウト作成 (#28)
    * ✨ next.js 初期設定
    
    * ✨ doctor 基本レイアウト作成

---
+++ front-end/src/app/doctor/dashboard/layout.tsx
@@ -0,0 +1,33 @@
+"use client";
+import { AppShell, Burger, Title } from "@mantine/core";
+import { useDisclosure } from "@mantine/hooks";
+import style from "./layout.module.scss";
+
+export default function Layout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  const [opened, { toggle }] = useDisclosure();
+
+  return (
+    <AppShell
+      header={{ height: 60 }}
+      navbar={{
+        width: 300,
+        breakpoint: "sm",
+        collapsed: { mobile: !opened },
+      }}
+      padding="md"
+    >
+      <AppShell.Header className={style.header}>
+        <Burger opened={opened} onClick={toggle} hiddenFrom="sm" size="sm" />
+        <Title order={2}>医心堂鍼灸整骨院</Title>
+      </AppShell.Header>
+
+      <AppShell.Navbar p="md">Navbar</AppShell.Navbar>
+
+      <AppShell.Main>{children}</AppShell.Main>
+    </AppShell>
+  );
+}
