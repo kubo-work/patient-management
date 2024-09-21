c046a3fe74dc464c9d4bf569329bd05637211eb8
kubo-work <kubowork0501@gmail.com>
2024-09-21 17:30:01 +0900

    ✨ 患者一覧画面バックエンドと接続して取得

---
+++ front-end/src/app/hooks/usePatientsList.ts
@@ -0,0 +1,29 @@
+import useSWR, { useSWRConfig } from "swr";
+import { API_URL } from "../../../constants/url";
+import { PatientType } from "../../../../common/types/PatientType";
+
+
+async function fetcher(key: string): Promise<PatientType[]> {
+    return fetch(key).then((res) => res.json());
+}
+
+export const usePatientsList = () => {
+    const { data, isLoading, error } = useSWR(
+        `${API_URL}/patients`,
+        fetcher
+    );
+
+    const { mutate } = useSWRConfig();
+
+    const doMutate = () => {
+        mutate(`${API_URL}/patients`);
+    }
+
+
+    return {
+        data,
+        isLoading,
+        error,
+        doMutate
+    }
+}
