60c1d127d1284126cf12c715f08bf20faeebc292
kubo-work <kubowork0501@gmail.com>
2024-09-21 17:38:37 +0900

    💄 性別カラム追加

---
+++ front-end/constants/sexList.ts
@@ -0,0 +1,16 @@
+import { SexTypes } from "../../common/types/SexTypes";
+
+export const sexList: SexTypes = {
+    no_answer: {
+        label: "未回答"
+    },
+    man: {
+        label: "男性"
+    },
+    woman: {
+        label: "女性"
+    },
+    neither: {
+        label: "その他"
+    }
+}
