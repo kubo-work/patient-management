a7922f3a5b17276f4b00f6ecc297af38d61ea74d
kubo-work <kubowork0501@gmail.com>
2024-09-29 18:09:42 +0900

    💄 診察日の表示形式を変更

--- frontend/src/app/features/doctor/medical_records/MedicalRecordsContents.tsx
+++ frontend/src/app/features/doctor/medical_records/MedicalRecordsContents.tsx
@@ -4,11 +4,17 @@ import React, { useMemo } from "react";
 import { MantineReactTable, MRT_ColumnDef } from "mantine-react-table";
 import { MedicalRecordsType } from "../../../../../../common/types/MedicalRecordsType";
 import { Box, Button, Flex, List, ListItem } from "@mantine/core";
+import dayjs from "dayjs";
+import utc from "dayjs/plugin/utc";
+import timezone from "dayjs/plugin/timezone";
 
 type Props = {
   name: string;
   patients_id: number;
 };
+
+dayjs.extend(utc);
+dayjs.extend(timezone);
 
 const MedicalRecordsContents = React.memo(({ name, patients_id }: Props) => {
   const { medicalRecord } = useMedicalRecords(patients_id);
@@ -17,6 +23,14 @@ const MedicalRecordsContents = React.memo(({ name, patients_id }: Props) => {
       {
         accessorKey: "examination_at",
         header: "診察日",
+        Cell: ({ row }) => {
+          const examination_at: dayjs.Dayjs = dayjs(
+            row.original.examination_at
+          ).utc();
+          const setTimeZone: dayjs.Dayjs = examination_at.tz("Asia/Tokyo");
+          const format: string = setTimeZone.format("YY年m月D日 H:i:s");
+          return format;
+        },
         maxSize: 200,
       },
       {
