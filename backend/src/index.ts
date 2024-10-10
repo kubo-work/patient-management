20d7f1cd596b1a070d4d188c7193b9411af88afe
kubo-work <kubowork0501@gmail.com>
2024-10-10 21:52:31 +0900

    🎨 環境変数の設定先を変更

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -39,8 +39,8 @@ app.use(session({
     saveUninitialized: false,
     name: sessionName,
     cookie: {
-        secure: false, // HTTPSを使用
-        httpOnly: process.env.SET_COOKIE_HTTP_ONLY === 'production',
+        secure: process.env.DOCTOR_SESSION_SECURE === "true", // HTTPSを使用
+        httpOnly: true, // XSS攻撃を防ぐ
         sameSite: 'lax',
         maxAge: 24 * 60 * 60 * 1000 // セッションの有効期限を設定（例: 24時間）
     }
