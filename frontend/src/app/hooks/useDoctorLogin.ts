67186b95b8eb1a5e0d801754f9a565884a18c16e
kubo-work <kubowork0501@gmail.com>
2024-10-10 20:32:03 +0900

    🔇 コンソールログ削除

--- frontend/src/app/hooks/useDoctorLogin.ts
+++ frontend/src/app/hooks/useDoctorLogin.ts
@@ -8,7 +8,6 @@ type FormValues = {
 }
 
 export const useDoctorLogin = () => {
-    console.log(API_URL)
     const router = useRouter();
     const [loginError, setLoginError] = useState<string>("");
     const handleLogin = async (values: FormValues) => {
