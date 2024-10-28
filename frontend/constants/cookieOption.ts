016d143cc38c6baeca82b10d3d03a8aa557847fd
kubo-work <kubowork0501@gmail.com>
2024-10-28 23:31:48 +0900

    🐛 sameSiteの型のエラー修正

--- frontend/constants/cookieOption.ts
+++ frontend/constants/cookieOption.ts
@@ -1,4 +1,4 @@
-export const doctorCookieOptions: { maxAge: number, path: string, secure: boolean, httpOnly: boolean, sameSite: string } = {
+export const doctorCookieOptions: { maxAge: number, path: string, secure: boolean, httpOnly: boolean, sameSite: boolean | "none" | "lax" | "strict" | undefined } = {
     maxAge: 1 * 24 * 60 * 60,
     path: '/doctor',
     secure: process.env.NEXT_DOCTOR_SESSION_SECURE === 'true', // HTTPSでのみ送信
