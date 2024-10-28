b6cc6fb83e44280915aeb0aeb0fde18f16e30cb1
kubo-work <kubowork0501@gmail.com>
2024-10-29 07:27:23 +0900

    🎨 next.config 修正

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,30 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    async headers() {
+        return [
+            {
+                source: "/doctor/:path*", // APIエンドポイントに対して設定
+                headers: [
+                    {
+                        key: "Access-Control-Allow-Origin",
+                        value: process.env.NEXT_PUBLIC_API_URL, // クロスドメインで許可するドメイン
+                    },
+                    {
+                        key: "Access-Control-Allow-Credentials",
+                        value: "true", // クッキーを使用するためtrueに設定
+                    },
+                    {
+                        key: "Access-Control-Allow-Methods",
+                        value: "GET,POST,PUT,OPTIONS",
+                    },
+                    {
+                        key: "Access-Control-Allow-Headers",
+                        value: "Content-Type,Authorization",
+                    },
+                ],
+            },
+        ];
+    }
+}
 
 export default nextConfig;
