acbdfd7a9f8404ed1177090daa65dd9ad1a235e8
kubo-work <kubowork0501@gmail.com>
2024-10-30 21:19:48 +0900

    ⏪ cookieの設定をnoneに戻す

--- frontend/constants/cookieOption.ts
+++ frontend/constants/cookieOption.ts
@@ -1,7 +1,6 @@
-export const doctorCookieOptions: { maxAge: number, path: string, secure: boolean, httpOnly: boolean, sameSite: boolean | "none" | "lax" | "strict" | undefined } = {
+export const doctorCookieOptions: { maxAge: number, path: string, secure: boolean, httpOnly: boolean } = {
     maxAge: 24 * 60 * 60 * 1000,
     path: '/doctor',
     secure: process.env.NEXT_DOCTOR_SESSION_SECURE === 'true', // HTTPSでのみ送信
     httpOnly: false,
-    sameSite: 'lax'
 }
