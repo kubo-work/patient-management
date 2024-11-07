15c86bfcfd265055e8f27604d9c736e8580eddc9
kubo-work <kubowork0501@gmail.com>
2024-11-07 20:29:50 +0900

    ⏪ クッキーのドメインの設定を解除する

--- frontend/constants/cookieOption.ts
+++ frontend/constants/cookieOption.ts
@@ -3,7 +3,7 @@ import { CookieOptions } from "../../common/types/CookieOptions";
 const cookieOptions: CookieOptions = {
     maxAge: 24 * 60 * 60 * 1000,
     path: '/doctor',
-    secure: process.env.NEXT_DOCTOR_SESSION_SECURE === 'production',
+    secure: process.env.NEXT_DOCTOR_SESSION_SECURE === 'true',
     httpOnly: false,
 }
 
