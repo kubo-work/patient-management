886b6f94e75f445d274e1e3b96ab0919db40b481
kubo-work <kubowork0501@gmail.com>
2025-01-12 16:52:58 +0900

    🎨 カテゴリ取得のコードzod化と医者データの取得コードの修正

--- backend/src/doctor/categories.ts
+++ backend/src/doctor/categories.ts
@@ -1,6 +1,22 @@
-import { Request, Response, Router } from "express";
+import { Response, Router } from "express";
 import { prisma } from "../prisma.js";
 import { verifyAuthToken } from "../verifyAuthToken.js";
+import { z } from "zod";
+
+const categorySchema = {
+    id: z.number(),
+    treatment: z.string(),
+};
+
+const getCategorySchema = z.object({
+    id: z.number(),
+    treatment: z.string(),
+    children: z.array(z.object(categorySchema)),
+})
+
+const getCategoriesSchema = z.array(getCategorySchema);
+
+type GetCategory = z.infer<typeof getCategorySchema>;
 
 const router = Router();
 
@@ -22,7 +38,8 @@ router.get("/", verifyAuthToken, async (_, response: Response) => {
                 parent_id: null
             }
         });
-        return response.json(allCategories);
+        const parseCategories: GetCategory[] = getCategoriesSchema.parse(allCategories);
+        return response.json(parseCategories);
     } catch (e) {
         return response.status(400).json({ error: "データの取得に失敗しました。" });
     }
--- backend/src/doctor/login_doctor.ts
+++ backend/src/doctor/login_doctor.ts
@@ -18,6 +18,12 @@ const router = Router();
 router.get("/", verifyAuthToken, async (request: Request, response: Response) => {
     try {
         const doctor: DoctorType = await prisma.doctors.findFirst({
+            select: {
+                id: true,
+                name: true,
+                email: true,
+                password: true
+            },
             where: {
                 AND: [
                     { id: Number(request.user.userId) }
