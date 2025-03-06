635da325534ab6a99d7c2906be53c0ab7196ddf3
kubo-work <kubowork0501@gmail.com>
2025-03-06 22:32:29 +0900

    🐛 ミドルウェア修正

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -2,20 +2,19 @@ import { NextRequest, NextResponse } from "next/server";
 import { doctorCookieName } from "../../common/util/CookieName";
 
 export function middleware(req: NextRequest) {
-    if (req.nextUrl.pathname.startsWith("/doctor")) {
-        // 何故か getCookie のオブジェクトの中身の変数を request と書くとエラーになるので req にする
+    const url = req.nextUrl;
+
+    if (url.pathname.startsWith("/doctor")) {
         const cookieHeader = req.headers.get("cookie");
+        // httpOnlyのクッキーは取得できないので注意
         const cookie = cookieHeader?.split("; ").find(c => c.startsWith(`${doctorCookieName}=`))?.split("=")[1];
-        if (req.nextUrl.pathname !== "/doctor/login") {
-            if (!cookie) {
-                return NextResponse.redirect(new URL('/doctor/login', req.url));
-            }
-        } else {
-            if (cookie) {
-                return NextResponse.redirect(new URL('/doctor/patients-list', req.url));
-            }
+
+        if (!cookie && url.pathname !== "/doctor/login") {
+            return NextResponse.redirect(new URL("/doctor/login", req.url));
+        }
+        if (cookie && url.pathname === "/doctor/login") {
+            return NextResponse.redirect(new URL("/doctor/patients-list", req.url));
         }
     }
     return NextResponse.next();
 }
-
