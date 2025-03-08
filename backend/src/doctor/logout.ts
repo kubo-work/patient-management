60f00c72e48c639d154decd0db452e08549ad719
kubo-work <kubowork0501@gmail.com>
2025-03-08 13:06:04 +0900

    🎨 ログアウト時処理の順番変更

--- backend/src/doctor/logout.ts
+++ backend/src/doctor/logout.ts
@@ -10,8 +10,8 @@ router.post("/", async (request: Request, response: Response) => {
                 console.error('Failed to destroy session:', err);
                 response.status(500).send('ログアウトに失敗しました。');
             } else {
-                response.clearCookie(sessionName); // クッキーの名前に合わせて指定
                 response.clearCookie(doctorCookieName);
+                response.clearCookie(sessionName); // クッキーの名前に合わせて指定
                 response.send('ログアウトしました。');
             }
         });
