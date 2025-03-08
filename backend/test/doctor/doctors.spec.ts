717e723c87579015b1a65abdc64f3f45c4ae00f0
kubo-work <kubowork0501@gmail.com>
2025-03-08 20:51:16 +0900

    🎨 テストコード修正

--- backend/test/doctor/doctors.spec.ts
+++ backend/test/doctor/doctors.spec.ts
@@ -5,6 +5,7 @@ import { mockSetDoctorData, testToken } from "./mockData/mockLoginDoctorData";
 import { faker } from "@faker-js/faker/.";
 import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
 import { Prisma } from "@prisma/client";
+import { doctorCookieName } from "../../../common/util/CookieName";
 
 const mockSendDoctorData = mockSetDoctorData.map((data) => {
     const { id, name, email, password } = data;
@@ -19,7 +20,7 @@ describe("全医者データ取得", () => {
     test("医者データの取得", async () => {
         prismaMock.doctors.findMany.mockResolvedValue(mockSetDoctorData);
         const response = await request(app).get("/doctor/doctors")
-            .set("Authorization", `Bearer ${testToken}`)
+            .set("Cookie", `${doctorCookieName}=${testToken}`)
         expect(response.status).toBe(200);
         expect(response.body).toEqual(mockSendDoctorData)
     })
@@ -32,7 +33,7 @@ describe("全医者データ取得", () => {
 
     test("医者データの取得の失敗 : データがない時", async () => {
         const response = await request(app).get("/doctor/doctors")
-            .set("Authorization", `Bearer ${testToken}`)
+            .set("Cookie", `${doctorCookieName}=${testToken}`)
         expect(response.status).toBe(400);
         expect(response.body.error).toBe("データの取得に失敗しました。");
     })
@@ -47,7 +48,7 @@ describe("指定の医者データ取得", () => {
     test("指定の医者データの取得 : 成功", async () => {
         prismaMock.doctors.findFirst.mockResolvedValueOnce(mockSetDoctorData[0])
         const response = await request(app).get(`/doctor/doctors/${mockSetDoctorData[0].id}`)
-            .set("Authorization", `Bearer ${testToken}`)
+            .set("Cookie", `${doctorCookieName}=${testToken}`)
         expect(response.status).toBe(200);
         expect(response.body).toEqual(mockSendDoctorData[0])
     })
@@ -60,7 +61,7 @@ describe("指定の医者データ取得", () => {
 
     test("医者データの取得の失敗 : データがない時", async () => {
         const response = await request(app).get(`/doctor/doctors/${mockSetDoctorData[0].id}`)
-            .set("Authorization", `Bearer ${testToken}`)
+            .set("Cookie", `${doctorCookieName}=${testToken}`)
         expect(response.status).toBe(404);
         expect(response.body.error).toBe("指定された医師が見つかりません。");
     })
@@ -78,7 +79,7 @@ describe("医者データ更新", () => {
             password: faker.internet.password()
         }
         const response = await request(app).put(`/doctor/doctors/${mockSetDoctorData[0].id}`)
-            .set("Authorization", `Bearer ${testToken}`)
+            .set("Cookie", `${doctorCookieName}=${testToken}`)
             .send(mockPutDoctorData);
         expect(response.status).toBe(200);
     })
@@ -89,7 +90,7 @@ describe("医者データ更新", () => {
             password: faker.internet.password()
         }
         const response = await request(app).put(`/doctor/doctors/${mockSetDoctorData[0].id}`)
-            .set("Authorization", `Bearer ${testToken}`)
+            .set("Cookie", `${doctorCookieName}=${testToken}`)
             .send(mockPutDoctorData);
         expect(response.status).toBe(400);
         expect(response.body.error).toBe("入力データが不正です。");
@@ -109,7 +110,7 @@ describe("医者データ更新", () => {
         }
         const id = faker.number.int({ min: 1000, max: 9999 })
         const response = await request(app).put(`/doctor/doctors/${id}`)
-            .set("Authorization", `Bearer ${testToken}`)
+            .set("Cookie", `${doctorCookieName}=${testToken}`)
             .send(mockPutDoctorData);
         expect(response.status).toBe(404);
         expect(response.body.error).toBe("指定された医師が見つかりません。");
@@ -128,7 +129,7 @@ describe("医者データ新規作成", () => {
             password: faker.internet.password()
         }
         const response = await request(app).post(`/doctor/doctors`)
-            .set("Authorization", `Bearer ${testToken}`)
+            .set("Cookie", `${doctorCookieName}=${testToken}`)
             .send(mockPutDoctorData);
         expect(response.status).toBe(200);
     })
@@ -139,7 +140,7 @@ describe("医者データ新規作成", () => {
             password: faker.internet.password()
         }
         const response = await request(app).put(`/doctor/doctors/${mockSetDoctorData[0].id}`)
-            .set("Authorization", `Bearer ${testToken}`)
+            .set("Cookie", `${doctorCookieName}=${testToken}`)
             .send(mockPutDoctorData);
         expect(response.status).toBe(400);
         expect(response.body.error).toBe("入力データが不正です。");
