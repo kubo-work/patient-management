e83788401fdca81ee08b5913eb80449ea25eccb3
kubo-work <kubowork0501@gmail.com>
2026-02-21 17:33:15 +0900

    Feature/terraform (#255)
    * ✨ terra form  aws と接続するためのファイル設定
    
    * 🎨 codebuild.tf にてtローカルからマイグレート可能にするためのブランチ指定
    
    * 🎨 aws側のビルドを migrate のみに変更と各 js ファイルに概要のコメント記載
    
    * 🎨 buildspec.yml ファイル修正
    
    * 🎨 build ymlファイル修正と aws 用で手動で作成した schema.prisma 削除
    
    * 🐛 認証アカウント設定ミスの修正
    
    * 🎨 aws configure の profile 修正
    
    * 🎨 terraform ファイル修正と codebuild のマイグレート時のログ出力設定
    
    * 🎨 rds とgithubの認証削除
    
    * 🔥 github actions で動かせるように設定
    
    * 🎨 GitHub Actions でマイグレートするためのファイル作成
    
    * 🐛 github actions に表示されないので修正
    
    * 🔥 Github Actions に表示されたので削除
    
    * 🎨 workflow が実行できないのでコメントを修正して反映を試みる
    
    * 🎨 node scripts ファイル名やコマンド名を rds から awsに変更
    
    * 🎨 aws 設定の追加と不要な github actions ファイル削除

--- infra/security_group.tf
+++ infra/security_group.tf
@@ -8,12 +8,11 @@ resource "aws_security_group" "rds_sg" {
   vpc_id = aws_vpc.main.id
 
   ingress {
-    from_port   = 5432
-    to_port     = 5432
-    protocol    = "tcp"
-    cidr_blocks = ["10.0.0.0/16"] # 仮。あとでApp Runner SGに変更
+    from_port       = 5432
+    to_port         = 5432
+    protocol        = "tcp"
+    security_groups = [aws_security_group.app_runner_sg.id]
   }
-
 }
 
 resource "aws_security_group" "app_runner_sg" {
@@ -39,30 +38,3 @@ resource "aws_security_group" "app_runner_sg" {
     Name = "${var.project}-${var.environment}-app-runner-sg"
   }
 }
-
-# resource "aws_security_group" "vpc_endpoint" {
-#   name        = "${var.project}-${var.environment}-vpc-endpoint-sg"
-#   vpc_id      = aws_vpc.main.id
-#   description = "Allow HTTPS inbound traffic,and HTTPS outbound traffic."
-#   tags = {
-#     Name = "${var.project}-${var.environment}-vpc-endpoint-sg"
-#   }
-# }
-
-# resource "aws_security_group_rule" "vpcep_in_https" {
-#   type              = "ingress"
-#   protocol          = "tcp"
-#   from_port         = 443
-#   to_port           = 443
-#   security_group_id = aws_security_group.vpc_endpoint.id
-#   cidr_blocks       = [aws_vpc.main.cidr_block]
-# }
-
-# resource "aws_security_group_rule" "vpcep_out_https" {
-#   type              = "egress"
-#   protocol          = "tcp"
-#   from_port         = 443
-#   to_port           = 443
-#   security_group_id = aws_security_group.vpc_endpoint.id
-#   cidr_blocks       = [aws_vpc.main.cidr_block]
-# }
